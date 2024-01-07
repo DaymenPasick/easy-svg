13c93a85e3868606e739d7b2e4290d79750ba45d
DaymensCode <Daymenscode@gmail.com>
2024-01-07 14:45:24 -0500

    set fs promise const variables in index.js" "

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 //this index file will hold code for actually invoking the svg render process
 //after the initial code is all done
 
+//setting const to utilize fs promises api.
+//this will support writing/reading of files and easier validation of data
+const { readFile, writeFile} = require('fs/promises');
+
 
