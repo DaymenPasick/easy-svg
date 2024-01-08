59a218f615c6736c2d9311287dbecb170d3671a7
DaymensCode <Daymenscode@gmail.com>
2024-01-08 13:42:52 -0500

    validateShapeResponse will return shapePrompt answer back as all lowercase

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const init = () => {
     //will make sure textPrompt is between 1-3 letters
     //will also turn response to all CAPS
     validateTextResponse(data.textPrompt)
-    console.log(validateTextResponse(data.textPrompt));
+    console.log("Shape text will be: " + validateTextResponse(data.textPrompt));
  })
 }
 
